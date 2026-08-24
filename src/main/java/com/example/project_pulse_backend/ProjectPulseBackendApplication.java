a19094c8cf851834b8068768af8060409a30b618
alex <hoangvanduc2504@gmail.com>
2026-08-24 20:19:09 +0700

    khoi tao du an

---
+++ src/main/java/com/example/project_pulse_backend/ProjectPulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_pulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPulseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_pulse_backend/ProjectPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_pulse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPulseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
